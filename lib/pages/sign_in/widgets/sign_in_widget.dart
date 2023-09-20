50ce2bdb359aa396dde7b9bc995db09498ce003f
Jean <acalto2@gmail.com>
2023-09-20 19:09:31 +0200

    23. Sign in screen and reusable buttons part 3

--- lib/pages/sign_in/widgets/sign_in_widget.dart
+++ lib/pages/sign_in/widgets/sign_in_widget.dart
@@ -26,24 +26,28 @@ AppBar buildAppBar() {
 // we need context for accessing bloc
 Widget buildThirdPartyLogin(BuildContext context) {
   return Container(
+    margin: EdgeInsets.only(
+      top: 40.h,
+      bottom: 20.h,
+    ),
     child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        GestureDetector(
-          onTap: () {},
-          child: SizedBox(
-            width: 40.w,
-            height: 40.w,
-            child: Image.asset('assets/icons/google.png'),
-          ),
-        ),  GestureDetector(
-          onTap: () {},
-          child: SizedBox(
-            width: 40.w,
-            height: 40.w,
-            child: Image.asset('assets/icons/apple.png'),
-          ),
-        ),
+        _reusableIcons('google'),
+        _reusableIcons('apple'),
+        _reusableIcons('facebook'),
       ],
+    ),
+  );
+}
+
+Widget _reusableIcons(String iconName) {
+  return GestureDetector(
+    onTap: () {},
+    child: SizedBox(
+      width: 40.w,
+      height: 40.w,
+      child: Image.asset('assets/icons/$iconName.png'),
     ),
   );
 }

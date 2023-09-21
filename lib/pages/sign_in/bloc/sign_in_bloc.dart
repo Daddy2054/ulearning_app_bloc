80163404abed76de9a5297945a005343ee2b1170
Jean <acalto2@gmail.com>
2023-09-21 16:15:34 +0200

    36. Sign in bloc and create blocs part 3

--- lib/pages/sign_in/bloc/sign_in_bloc.dart
+++ lib/pages/sign_in/bloc/sign_in_bloc.dart
@@ -1,13 +1,17 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'sign_in_event.dart';
 part 'sign_in_state.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
-  SignInBloc() : super(SignInInitial()) {
-    on<SignInEvent>((event, emit) {
+  SignInBloc() : super(const SignInState()) {
+    on<EmailEvent>((event, state ) {
       // TODO: implement event handler
     });
+        on<PasswordEvent>((event, state ) {
+      // TODO: implement event handler
+    });
+
   }
 }

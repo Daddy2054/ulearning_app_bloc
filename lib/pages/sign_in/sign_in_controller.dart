6620d16e64b1e1c1328697009fe00699af7fe952
Jean <acalto2@gmail.com>
2023-09-21 17:06:58 +0200

    41. Signin controller, firebase auth part 2

--- lib/pages/sign_in/sign_in_controller.dart
+++ lib/pages/sign_in/sign_in_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ulearning_app_bloc/pages/sign_in/bloc/sign_in_bloc.dart';
@@ -7,10 +8,21 @@ class SignInController {
 
   SignInController({required this.context});
 
-  void handleSignIn(String type) {
+  void handleSignIn(String type) async {
     try {
       if (type == 'email') {
         final state = context.read<SignInBloc>().state;
+        String emailAddress = state.email;
+        String password = state.password;
+        if (emailAddress.isEmpty) {}
+        if (password.isEmpty) {}
+        try {
+          final credential =
+              await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: emailAddress,
+            password: password,
+          );
+        } catch (e) {}
       }
     } catch (e) {}
   }

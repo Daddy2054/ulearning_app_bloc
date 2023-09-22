e6b26c7331559d56c952fa3dfeddbf6666ef2c07
Jean <acalto2@gmail.com>
2023-09-22 18:45:22 +0200

    60. Register, optional named param. for blocs p10

--- lib/pages/register/bloc/register_bloc.dart
+++ lib/pages/register/bloc/register_bloc.dart
@@ -5,9 +5,27 @@ part 'register_event.dart';
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(RegisterState()) {
-    on<RegisterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+  RegisterBloc() : super(const RegisterState()) {
+     on<UserNameEvent>(_userNameEvent);
+     on<EmailEvent>(_emailEvent);
+     on<PasswordEvent>(_passwordEvent);
+     on<RePasswordEvent>(_rePasswordEvent);
+   }
+
+   void _userNameEvent(UserNameEvent event, Emitter<RegisterState> emit){
+     print("${event.userName}");
+      emit(state.copyWith(userName:event.userName));
+   }
+   void _emailEvent(EmailEvent event, Emitter<RegisterState> emit){
+     print("${event.email}");
+      emit(state.copyWith(email:event.email));
+   }
+   void _passwordEvent(PasswordEvent event, Emitter<RegisterState> emit){
+     print("${event.password}");
+      emit(state.copyWith(password:event.password));
+   }
+   void _rePasswordEvent(RePasswordEvent event, Emitter<RegisterState> emit){
+     print("${event.rePassword}");
+      emit(state.copyWith(rePassword:event.rePassword));
+   }
 }

fe9dcd79f020bd833af7b11191e80f577826d173
Jean <acalto2@gmail.com>
2023-09-22 20:29:33 +0200

    65. Register controller and part 2

--- lib/pages/register/register_controller.dart
+++ lib/pages/register/register_controller.dart
@@ -7,43 +7,66 @@ import '../../common/widgets/flutter_toast.dart';
 
 class RegisterController {
   final BuildContext context;
-  const RegisterController({required this.context});
+  const RegisterController({
+    required this.context,
+  });
 
+  Future<void> handleEmailRegister() async {
+    final state = context.read<RegisterBloc>().state;
 
-void handleEmailRegister() {
-  final state = context.read<RegisterBloc>().state;
-  String userName = state.userName;
-  String email = state.email;
-  String password = state.password;
-  String rePassword = state.rePassword;
+    String userName = state.userName;
+    String email = state.email;
+    String password = state.password;
+    String rePassword = state.rePassword;
 
-  if (userName.isEmpty) {
-    toastInfo(msg: "User name can not be empty");
-    return;
-  }
-  if (email.isEmpty) {
-    toastInfo(msg: "Email name can not be empty");
-    return;
-  }
-  if (password.isEmpty) {
-    toastInfo(msg: "Password name can not be empty");
-    return;
-  }
-  if (rePassword.isEmpty) {
-    toastInfo(msg: "Your password confirmation is wrong");
-    return;
-  }
+    if (userName.isEmpty) {
+      toastInfo(msg: "User name can not be empty");
+      return;
+    }
+    if (email.isEmpty) {
+      toastInfo(msg: "Email name can not be empty");
+      return;
+    }
+    if (password.isEmpty) {
+      toastInfo(msg: "Password name can not be empty");
+      return;
+    }
+    if (rePassword.isEmpty) {
+      toastInfo(msg: "Your password confirmation is wrong");
+      return;
+    }
 
-  try {
-    
-  } on FirebaseAuthException catch (e) {
-      if(e.code == 'weak-password'){
+    try {
+    var context2 = Navigator.of(context); // how to jump async gap step 1
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      if (credential.user != null) {
+        await credential.user?.sendEmailVerification();
+        await credential.user?.updateDisplayName(userName);
+        String photoUrl = "uploads/default.png";
+        await credential.user?.updatePhotoURL(photoUrl);
+
+        toastInfo(
+            msg:
+                """An email has been sent to your registered email. 
+                To activate it please check your email box 
+                and click on the link"""
+                );
+        // Navigator.of(context).pop();
+        (context2).pop(); // how to jump async gap step 2
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
         toastInfo(msg: "The password provided is too weak");
-      }else if(e.code == 'email-already-in-use'){
+      } else if (e.code == 'email-already-in-use') {
         toastInfo(msg: "The email is already in use");
-      }else if(e.code == 'invalid-email'){
+      } else if (e.code == 'invalid-email') {
         toastInfo(msg: "Your email id is invalid");
       }
+    }
   }
-}
 }

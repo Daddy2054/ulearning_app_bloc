60e6fcb7d6c8f4fe26e68436d15f0b7eca78f04d
Jean <acalto2@gmail.com>
2023-09-22 18:34:41 +0200

    59. Register screen and register blocs part 9

--- lib/pages/register/bloc/register_event.dart
+++ lib/pages/register/bloc/register_event.dart
@@ -1,5 +1,27 @@
 part of 'register_bloc.dart';
 
-// @immutable
-// sealed 
-class RegisterEvent {}
+abstract class RegisterEvent {
+  const RegisterEvent();
+}
+
+class UserNameEvent extends RegisterEvent{
+  final String userName;
+  const UserNameEvent(this.userName);
+
+}
+class EmailEvent extends RegisterEvent{
+  final String email;
+  const EmailEvent(this.email);
+
+}
+class PasswordEvent extends RegisterEvent{
+  final String password;
+  const PasswordEvent(this.password);
+
+}
+
+class RePasswordEvent extends RegisterEvent{
+  final String rePassword;
+  const RePasswordEvent(this.rePassword);
+
+}

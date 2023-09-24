b83f12bad974ad9411a43d671c802018b3dbddb8
Jean <acalto2@gmail.com>
2023-09-24 20:54:15 +0200

    90. SharedPreferences, remember welcome screen p4

--- lib/common/routes/pages.dart
+++ lib/common/routes/pages.dart
@@ -63,21 +63,26 @@ class AppPages {
       //check for route name macthing when navigator gets triggered.
       var result = routes().where((element) => element.route == settings.name);
       if (result.isNotEmpty) {
-        // if (kDebugMode) {
-        //   print('valid route name is: ${settings.name}');
-        // }
+        if (kDebugMode) {
+//          print('valid route name is: ${settings.name}');
+          print('first log');
+          print(result.first.route);
+        }
         bool deviceFirstOpen = Global.storageService.getDeviceFirstOpen();
         if (result.first.route == AppRoutes.INITIAL && deviceFirstOpen) {
-          //bool isLoggedin = Global.storageService.getIsLoggedIn();
+            if (kDebugMode) {
+//          print('valid route name is: ${settings.name}');
+          print('second log');
+        }  //bool isLoggedin = Global.storageService.getIsLoggedIn();
           // if(isLoggedin){
           //   return MaterialPageRoute(builder: (_)=>const ApplicationPage(), settings: settings);
           // }
 
-          //   return MaterialPageRoute(builder: (_)=>const SignIn(), settings:settings);
-
           return MaterialPageRoute(
-              builder: (_) => result.first.page, settings: settings);
+              builder: (_) => const SignIn(), settings: settings);
         }
+        return MaterialPageRoute(
+            builder: (_) => result.first.page, settings: settings);
       }
     }
     if (kDebugMode) {

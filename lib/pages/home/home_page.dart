08402578c8a7e776d3d7a33a0b7dcc20b163ff56
Jean <acalto2@gmail.com>
2023-09-25 19:16:19 +0200

    93. Home page and title text part 3

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app_bloc/common/values/colors.dart';
 import 'package:ulearning_app_bloc/pages/home/widgets/home_page_widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,9 +13,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: buildAppBar(),
+      body: Container(
+        margin: EdgeInsets.symmetric(
+          vertical: 0,
+          horizontal: 25.w,
+        ),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20.h),
+              child: Text(
+                'hello',
+                style: TextStyle(
+                  color: AppColors.primaryThirdElementText,
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
 //      appBar: buildAppBar(userProfile.avatar.toString()),
     );
   }

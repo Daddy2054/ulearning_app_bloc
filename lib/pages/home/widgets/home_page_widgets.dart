27ce3690c65a242979a63f82d775bc0db065e3df
Jean <acalto2@gmail.com>
2023-09-25 19:07:40 +0200

    92. Home page and app bar part 2

--- lib/pages/home/widgets/home_page_widgets.dart
+++ lib/pages/home/widgets/home_page_widgets.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 AppBar buildAppBar() {
-  return AppBar();
+  return AppBar(
+    title: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 15.w,
+            height: 12.h,
+            child: const Icon(Icons.menu),
+          ),
+          GestureDetector(
+            child: Container(
+              width: 40.w,
+              height: 40.h,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icons/person.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }

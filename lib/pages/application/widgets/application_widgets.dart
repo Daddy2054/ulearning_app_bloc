7331be52f2e4b03500be2481af1cf9cb92031e58
Jean <acalto2@gmail.com>
2023-09-26 19:58:58 +0200

    116. Profile page and create page part 1

--- lib/pages/application/widgets/application_widgets.dart
+++ lib/pages/application/widgets/application_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ulearning_app_bloc/pages/home/home_page.dart';
+import 'package:ulearning_app_bloc/pages/profile/widgets/profile.dart';
 
 Widget buildPage(int index) {
   List<Widget> widget = [
@@ -7,7 +8,7 @@ Widget buildPage(int index) {
     const Text('Search'),
     const Text('Course'),
     const Text('chat'),
-    const Text('profile'),
+    const ProfilePage(),
   ];
 
   return widget[index];
---
+++ lib/pages/profile/widgets/profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('data'),),
+    );
+  }
+}

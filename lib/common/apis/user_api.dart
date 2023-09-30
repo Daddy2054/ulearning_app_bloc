4f2d435799140cbf2301f36600fe5d27f32032c7
Jean <acalto2@gmail.com>
2023-09-30 18:37:45 +0200

    147. Front end and work on the end point part 3

---
+++ lib/common/apis/user_api.dart
@@ -0,0 +1,14 @@
+
+import '../entities/user.dart';
+
+class UserAPI{
+ static login({LoginRequestEntity? params}) async {
+   //response = response.data after the post method returns
+    // var response = await HttpUtil().post(
+    //   'api/login',
+    //   queryParameters:params?.toJson()
+    // );
+
+  //  return UserLoginResponseEntity.fromJson(response);
+  }
+}

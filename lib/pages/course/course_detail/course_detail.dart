17e6ed049246e8c36fcd0a2d1a5610e5bf2b72a8
Jean <acalto2@gmail.com>
2023-10-07 20:17:40 +0200

    202. Detail page and draw text and button part 9

--- lib/pages/course/course_detail/course_detail.dart
+++ lib/pages/course/course_detail/course_detail.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_app_bloc/pages/course/course_detail/bloc/course_detail_states.dart';
 import 'package:ulearning_app_bloc/pages/course/course_detail/widgets/course_detail_widgets.dart';
 
+import '../../../common/values/colors.dart';
 import '../../../common/widgets/base_text_widget.dart';
 
 class CourseDetail extends StatefulWidget {
@@ -48,15 +49,48 @@ class _CourseDetailState extends State<CourseDetail> {
                       horizontal: 25.w,
                     ),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         thumbNail(
                           'images/86b887e108e25d8b4afb4c2d93b61b3c.jpeg',
                         ),
 //                        menuView(context, CourseDetailStates state)
                         const SizedBox(
-                          height: 10,
+                          height: 15,
                         ),
                         menuView(context),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        reusableText('Course Description'),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        reusableText(
+                            'Course Description Course Description Course Description Course Description Course Description Course Description Course Description ',
+                            color: AppColors.primaryThirdElementText,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 11.sp,),
+                               //course buy button
+                            GestureDetector(
+                              onTap: (){
+                       //         _courseDetailController.goBuy(state.courseItem!.id);
+                              },
+                              child: appPrimaryButton("Go buy"),
+                            ),
+
+                            SizedBox(height: 20.h,),
+                            //course summary title
+                            courseSummaryTitle(),
+
+                            //course summary in list
+                         //   courseSummaryView(context, state),
+                            SizedBox(height: 20.h,),
+                            //Lesson list title
+                            reusableText("Lesson List"),
+                            SizedBox(height: 20.h,),
+                            //Course lesson list
+                            //courseLessonList(state)
                       ],
                     ),
                   ),
